eb3926e65273791de29a0ec5b50657c4a8fad6d1
melkoff <vasamelko@gmail.com>
2023-03-09 11:17:14 +0200

    2

--- src/js/basket.js
+++ src/js/basket.js
@@ -1,36 +1,6 @@
-let modal = document.getElementById("myModal");
-let btn = document.getElementById("openModal");
-let closeBtn = document.getElementsByClassName("closeModal")[0];
-let hide = document.getElementById("hideModal");
-let bodyClose = document.querySelector('html');
-// const modal = document.querySelector('html');
-
-hide.addEventListener('click', bodyScroll);
-
-function bodyScroll() {
-   modal.classList.toggle("menu__open");
-   bodyClose.classList.toggle("menu__close");
-}
 
 
 
-
-btn.onclick = function () {
-   modal.style.display = "block";
-   btn.style.display = "none";
-}
-
-closeBtn.onclick = function () {
-   modal.style.display = "none";
-   btn.style.display = "block";
-}
-
-window.onclick = function (event) {
-   if (event.target == modal) {
-      modal.style.display = "none";
-      btn.style.display = "block";
-   }
-}
 
 
 
